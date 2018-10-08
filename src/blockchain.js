af1875bb3bc0bc55a56ccc424d46ff93716b9a4d
brenj <brenj@mailbox.org>
2018-10-08 13:52:56 -0500

    Hardcode Genesis block

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,6 +1,21 @@
 const Block = require('./block.js');
 const blockchainData = require('./blockchainData.js');
 
+const GENESIS_BLOCK = {
+  body:
+   { address: '19AAjaTUbRjQCMuVczepkoPswiZRhjtg31',
+     star:
+      { dec: '-26° 29\' 24.9',
+        ra: '16h 29m 1.0s',
+        story: '47656E6573697320426C6F636B',
+      },
+   },
+  height: 0,
+  previousBlockHash: '',
+  time: '1539023948',
+  hash: 'cf0d39b631ecfde41feb7599ef06fbbffe47197eaff949547698d8ef04af9b6d',
+};
+
 class Blockchain {
   constructor() {
     this.api = blockchainData;
