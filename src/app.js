c5e5f9778e96fd5a14eeea34033b441a779ddad1
brenj <brenj@mailbox.org>
2018-10-09 13:34:48 -0500

    Cleanup use declaration for controllers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
+const controllers = require('./controllers');
 const middlewares = require('./middlewares');
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
-app.use(require('./controllers'));
+app.use(controllers);
 app.use(middlewares.defaultErrorHandler);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
