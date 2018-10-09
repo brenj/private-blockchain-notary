085125df6ec22d50895df0af6eabd8244bc937a0
brenj <brenj@mailbox.org>
2018-10-09 13:35:23 -0500

    Prune star request data every 15 minutes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const controllers = require('./controllers');
 const middlewares = require('./middlewares');
+const starRequestData = require('./models/starRequestData.js');
 
 const app = express();
 const PORT = 8000;
+const PRUNE_REQUESTS_TIMEOUT = 900000; // 15 minutes
 
 app.use(express.json());
 app.use(controllers);
 app.use(middlewares.defaultErrorHandler);
+
+setInterval(starRequestData.pruneExpiredRequests, PRUNE_REQUESTS_TIMEOUT);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
